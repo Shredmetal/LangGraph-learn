cc3cae0dd2ab5bc525072630b74161579be0f83a
Morgan Lee <morgan.lee@synpulse.com>
2025-03-26 20:26:44 +0800

    reflexion agent

---
+++ reflexion_agent/reflexion_schemas.py
@@ -0,0 +1,20 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing.")
+    superfluous: str = Field(description="Critique of what is superfluous.")
+
+class AnswerQuestion(BaseModel):
+    answer: str = Field(description="~250 word detailed answer to the question.")
+    reflections: Reflection = Field(description="Your reflection on the initial answer.")
+    search_queries: List[str] = Field(
+        description="1-3 search queries for researching improvements to address the critique of your current answer."
+    )
+    id: str = Field(description="Id of the response")
+
+class ReviseAnswer(AnswerQuestion):
+    references: List[str] = Field(
+        description="Citations for information added in your updated answer."
+    )
